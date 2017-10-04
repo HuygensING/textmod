1c49af3fe0007f5a72b5abf25a38e0037634b8fd
HDJ <hayco.de.jong@huygens.knaw.nl>
2017-10-04 11:33:54 +0200

    Add rudimentary Server

--- src/main/java/nl/knaw/huygens/topmod/Server.java
+++ src/main/java/nl/knaw/huygens/topmod/Server.java
@@ -1,4 +1,25 @@
-package nl.knaw.huygens.topmod.resources;
+package nl.knaw.huygens.topmod;
 
-public class Server {
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Environment;
+import nl.knaw.huygens.topmod.resources.KeywordSuggestResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server extends Application<Server.Config> {
+  private static final Logger LOG = LoggerFactory.getLogger(Server.class);
+
+  static class Config extends Configuration {
+  }
+
+  public static void main(String[] args) throws Exception {
+    new Server().run(args);
+  }
+
+  @Override
+  public void run(Config config, Environment environment) throws Exception {
+    LOG.debug("running with config: {}", config);
+    environment.jersey().register(new KeywordSuggestResource());
+  }
 }
