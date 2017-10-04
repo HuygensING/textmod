e79d02ed6df7d3024506ca892a3580631ca4071f
walter <walter.ravenek@huygens.knaw.nl>
2017-10-04 14:24:25 +0200

    Use model class for returning suggestion

---
+++ src/main/java/nl/knaw/huygens/topmod/api/Suggestion.java
@@ -0,0 +1,34 @@
+package nl.knaw.huygens.topmod.api;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Terms suggested by topic model.
+ */
+public class Suggestion {
+
+  private List<String> terms;
+
+  public Suggestion() {
+    this(Collections.emptyList());
+  }
+
+  public Suggestion(List<String> terms) {
+    setTerms(terms);
+  }
+
+  public Suggestion(String... terms) {
+    this(Arrays.asList(terms));
+  }
+
+  public List<String> getTerms() {
+    return terms;
+  }
+
+  public void setTerms(List<String> terms) {
+    this.terms = Collections.unmodifiableList(terms);
+  }
+
+}
--- src/main/java/nl/knaw/huygens/topmod/resources/KeywordSuggestResource.java
+++ src/main/java/nl/knaw/huygens/topmod/resources/KeywordSuggestResource.java
@@ -1,16 +1,17 @@
 package nl.knaw.huygens.topmod.resources;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.Arrays;
-import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import nl.knaw.huygens.topmod.api.Suggestion;
 
 @Path("suggest")
 @Produces(MediaType.APPLICATION_JSON)
@@ -19,9 +20,9 @@ public class KeywordSuggestResource {
 
   @POST
   @Consumes(MediaType.APPLICATION_JSON)
-  public List<String> getKeywordSuggestions(List<String> query) {
+  public Suggestion getKeywordSuggestions(List<String> query) {
     LOG.debug("Getting keyword suggestions for: {}", query);
-    return Arrays.asList("lorem", "ipsum");
+    return new Suggestion("lorem", "ipsum");
   }
 
 }
