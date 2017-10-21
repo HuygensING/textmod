c367926d3758ccc54136f830674113925b1e0594
Walter Ravenek <walter.ravenek@huygens.knaw.nl>
2017-10-21 14:06:19 +0200

    Add utility method for parsing a string

--- src/main/java/nl/knaw/huygens/topmod/core/text/TextAnalyzer.java
+++ src/main/java/nl/knaw/huygens/topmod/core/text/TextAnalyzer.java
@@ -1,11 +1,12 @@
 package nl.knaw.huygens.topmod.core.text;
 
-import java.io.IOException;
-import java.io.StringReader;
-
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
 
 /**
  * Base class for analyzing text token by token.
@@ -17,12 +18,17 @@ public class TextAnalyzer<T extends TokenTextHandler> {
     return new TextAnalyzer<U>(luceneAnalyzer, handler);
   }
 
-  public static <U extends TokenTextHandler> void analyze(Analyzer luceneAnalyzer, U handler, String text) {
+  public static <U extends TokenTextHandler> U analyze(Analyzer luceneAnalyzer, U handler, String text) {
     try {
-      TextAnalyzer.newInstance(luceneAnalyzer, handler).analyze(text);
+      newInstance(luceneAnalyzer, handler).analyze(text);
+      return handler;
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  public static List<String> parse(Analyzer luceneAnalyzer, String text) {
+    return analyze(luceneAnalyzer, new ListTokenTextHandler(), text).getTokens();
   }
 
   private final Analyzer luceneAnalyzer;
@@ -38,16 +44,16 @@ public class TextAnalyzer<T extends TokenTextHandler> {
   }
 
   public void analyze(String segmentId, String text) throws IOException {
-    TokenStream stream = luceneAnalyzer.tokenStream(null, new StringReader(text));
-    CharTermAttribute attribute = stream.getAttribute(CharTermAttribute.class);
-    stream.reset();
-    tokenTextHandler.newSegment(segmentId);
-    while (stream.incrementToken()) {
-      String term = new String(attribute.buffer(), 0, attribute.length());
-      tokenTextHandler.handle(term);
+    try (TokenStream stream = luceneAnalyzer.tokenStream(null, new StringReader(text))) {
+      CharTermAttribute attribute = stream.getAttribute(CharTermAttribute.class);
+      stream.reset();
+      tokenTextHandler.newSegment(segmentId);
+      while (stream.incrementToken()) {
+        String term = new String(attribute.buffer(), 0, attribute.length());
+        tokenTextHandler.handle(term);
+      }
+      tokenTextHandler.endSegment();
     }
-    tokenTextHandler.endSegment();
-    stream.close();
   }
 
   public void analyze(String text) throws IOException {
--- src/test/java/nl/knaw/huygens/topmod/core/text/TextAnalyzerTest.java
+++ src/test/java/nl/knaw/huygens/topmod/core/text/TextAnalyzerTest.java
@@ -1,22 +1,17 @@
 package nl.knaw.huygens.topmod.core.text;
 
-import java.io.IOException;
-import java.util.List;
-
-import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.SimpleAnalyzer;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.List;
+
 public class TextAnalyzerTest {
 
   @Test
-  public void testSimpleText() throws IOException {
-    Analyzer luceneAnalyzer = new SimpleAnalyzer();
-    ListTokenTextHandler handler = new ListTokenTextHandler();
-    TextAnalyzer.newInstance(luceneAnalyzer, handler).analyze("\t Hugo de Groot \n");
-
-    List<String> tokens = handler.getTokens();
+  public void testParsing() throws IOException {
+    List<String> tokens = TextAnalyzer.parse(new SimpleAnalyzer(), "\t Hugo de Groot \n");
     Assert.assertEquals(3, tokens.size());
     Assert.assertEquals("hugo", tokens.get(0));
     Assert.assertEquals("de", tokens.get(1));
